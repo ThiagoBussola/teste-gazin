4ebfcbb23b2ea09dc2939a4b3cdfbe7f6faa2f31
Thiago <thiago.bussola@tindin.com.br>
2021-09-11 14:52:20 -0300

    adicionando cobertura de testes

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,12 @@ module.exports = {
   },
   clearMocks: true,
   coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
   testPathIgnorePatterns: ['./dist/*']
 }
