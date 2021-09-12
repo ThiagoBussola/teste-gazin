8beb7a4c60b3e831bb77f8268e31561ed4db8a3e
Thiago <thiago.bussola@tindin.com.br>
2021-09-11 23:33:32 -0300

    ajustando docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:16.9.0-slim
 WORKDIR /app
-COPY ["package.json", "package-lock.json*", "./"]
+COPY ["package.json", "package-lock.json*", "./" ]
 RUN npm install
 COPY . .
 CMD ["npm", "start"]
--- common/services/mongoose.service.ts
+++ common/services/mongoose.service.ts
@@ -4,11 +4,12 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const log: debug.IDebugger = debug('app:mongoose-service')
-const {
-  MONGO_HOSTNAME,
-  MONGO_PORT,
-  MONGO_DB
-} = process.env
+// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME || 'localhost'
+// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+const MONGO_PORT = process.env.MONGO_PORT || 27017
+// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+const MONGO_DB = process.env.MONGO_DB || 'api-db-gazin'
 class MongooseService {
   private count = 0
   private readonly mongooseOptions = {
@@ -28,8 +29,6 @@ class MongooseService {
   connectWithRetry = () => {
     log('Attempting MongoDB connection (will retry if needed)')
 
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    console.log(`mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`)
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
     mongoose.connect(`mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`, this.mongooseOptions).then(() => {
       log('MongoDB is connected')
